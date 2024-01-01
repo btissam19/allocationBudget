b195f27ba6d7b51ef7df37ae2926f43f0b6b915a
Ibtissam <btissam1234btissam12@gmail.com>
2024-01-01 13:16:09 +0100

    last version

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { useContextcustum } from '../contexts/AppContext'
+import React, { useState, useEffect } from 'react';
+import { useContextcustum } from '../contexts/AppContext';
+
 const Budget = () => {
-  const { budget ,currency } = useContextcustum()
+  const { budget, currency, dispatch } = useContextcustum();
+  const [editableBudget, setEditableBudget] = useState(budget);
+
+  useEffect(() => {
+    setEditableBudget(budget);
+  }, [budget]);
+
+  const handleBudgetChange = (e) => {
+    const newBudget = parseInt(e.target.value, 10);
+    setEditableBudget(newBudget);
+
+    // Dispatch an action to update the budget in the context
+    dispatch({
+      type: 'CHG_BUDGET',
+      payload: newBudget,
+    });
+  };
+
   return (
     <div className="alert alert-secondary">
-      <span> Budget: {currency}{budget} </span>{' '}
+      <span> Budget: {currency}</span>
+      <input
+        type="number"
+        value={editableBudget}
+        onChange={handleBudgetChange}
+      />
     </div>
-  )
-}
-export default Budget
+  );
+};
+
+export default Budget;
